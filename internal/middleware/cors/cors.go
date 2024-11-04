77301866b2cab1743453745c446f2e04ead5c60b
Azamat Faskhutdinov <azfaskhutdinov@ozon.ru>
2024-11-05 01:17:33 +0300

    [FE-61]: добавил корс

--- internal/middleware/cors/cors.go
+++ internal/middleware/cors/cors.go
@@ -1,18 +1,33 @@
 package cors
 
 import (
-	"github.com/gorilla/handlers"
 	"net/http"
 )
 
+const allowedOrigin = "http://83.166.237.142"
+
 func CorsMW(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		handlers.CORS(
-			handlers.AllowedOrigins([]string{"http://83.166.237.142"}),
-			handlers.AllowedHeaders([]string{"Content-Type", "X-Auth-Token"}),
-			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "OPTIONS", "HEAD", "DELETE"}),
-			handlers.AllowCredentials(),
-		)
+		w.Header().Add("Access-Control-Allow-Origin", allowedOrigin)
+		if r.Method == http.MethodOptions {
+			w.Header().Add("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		if r.Method == http.MethodGet {
+			w.Header().Set("Content-Type", "application/json")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+// CorsCredentionalsMiddleware provides header allow credentials
+func CorsCredentionalsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		next.ServeHTTP(w, r)
 	})
 }
