f31a562413e369b8f2049394cd634a7bb39705cb
Azamatik <azamat.faskhutdinov@vkteam.ru>
2024-12-10 21:54:50 +0300

    поправил дату рождения

--- internal/handlers/user/dto.go
+++ internal/handlers/user/dto.go
@@ -6,22 +6,24 @@ import (
 )
 
 type createUserRequest struct {
-	Username string    `json:"username"`
-	Password string    `json:"password"`
-	Phone    string    `json:"phone"`
-	Icon     []byte    `json:"icon"`
-	Birthday time.Time `json:"birthday"`
-	Gender   string    `json:"gender"`
-	IsAdmin  bool      `json:"isAdmin"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Phone    string `json:"phone"`
+	Icon     []byte `json:"icon"`
+	Birthday string `json:"birthday"`
+	Gender   string `json:"gender"`
+	IsAdmin  bool   `json:"isAdmin"`
 }
 
 func (r *createUserRequest) ToModel() user.User {
+	birthday, _ := time.Parse("2006-01-02", r.Birthday)
+
 	return user.User{
 		Username:  r.Username,
 		Password:  r.Password,
 		Phone:     r.Phone,
 		Icon:      r.Icon,
-		BirthDate: r.Birthday,
+		BirthDate: birthday,
 		Gender:    r.Gender,
 		IsAdmin:   r.IsAdmin,
 	}
